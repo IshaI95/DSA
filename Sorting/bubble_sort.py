12b787018f7f81b22ecd7b0ff465fcc00f676a39
IshaI95 <ishme2107@gmail.com>
2022-08-31 23:58:10 +0530

    Bubble Sort Algo

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,1 +1,24 @@
+# i = number of sorting iterations
+# j = iterator for list
+# After every iteration the highest values settles down at the end of the array.
+# Hence, the next iteration need not include already sorted elements.
 
+def bubbleSort(lst):
+    n = len(lst)
+
+    # Flag to confirm if list is already sorted and no swaps are required
+    swapped = False
+
+    for i in range(n -1):
+        for j in range(0, n -i -1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+                swapped = True
+
+        # Exit when no swaps are required
+        if not swapped:
+            return
+    return lst
+
+list = [90, 13, 27, 21, 35, 10]
+print(bubbleSort(list))
